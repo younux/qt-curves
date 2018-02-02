b09da492430573c00b68a2c71f3dbe6af8956cbc
younux <benhoumich.younes@gmail.com>
2018-02-02 20:57:04 +0100

    Add mac os x build

--- build-qt-curves-Desktop_Qt_5_10_0_clang_64bit-Debug/moc_mainwindow.cpp
+++ build-qt-curves-Desktop_Qt_5_10_0_clang_64bit-Debug/moc_mainwindow.cpp
@@ -21,8 +21,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_MainWindow_t {
-    QByteArrayData data[6];
-    char stringdata0[97];
+    QByteArrayData data[17];
+    char stringdata0[338];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -36,12 +36,30 @@ QT_MOC_LITERAL(1, 11, 21), // "on_btnAstroid_clicked"
 QT_MOC_LITERAL(2, 33, 0), // ""
 QT_MOC_LITERAL(3, 34, 21), // "on_btnCycloid_clicked"
 QT_MOC_LITERAL(4, 56, 21), // "on_btnHuygens_clicked"
-QT_MOC_LITERAL(5, 78, 18) // "on_btnHypo_clicked"
+QT_MOC_LITERAL(5, 78, 18), // "on_btnHypo_clicked"
+QT_MOC_LITERAL(6, 97, 18), // "on_btnLine_clicked"
+QT_MOC_LITERAL(7, 116, 25), // "on_spinScale_valueChanged"
+QT_MOC_LITERAL(8, 142, 4), // "arg1"
+QT_MOC_LITERAL(9, 147, 28), // "on_spinInterval_valueChanged"
+QT_MOC_LITERAL(10, 176, 25), // "on_spinCount_valueChanged"
+QT_MOC_LITERAL(11, 202, 29), // "on_btnBackgroundColor_clicked"
+QT_MOC_LITERAL(12, 232, 23), // "on_btnLineColor_clicked"
+QT_MOC_LITERAL(13, 256, 20), // "on_btnCircle_clicked"
+QT_MOC_LITERAL(14, 277, 21), // "on_btnEllipse_clicked"
+QT_MOC_LITERAL(15, 299, 19), // "on_btnFunny_clicked"
+QT_MOC_LITERAL(16, 319, 18) // "on_btnStar_clicked"
 
     },
     "MainWindow\0on_btnAstroid_clicked\0\0"
     "on_btnCycloid_clicked\0on_btnHuygens_clicked\0"
-    "on_btnHypo_clicked"
+    "on_btnHypo_clicked\0on_btnLine_clicked\0"
+    "on_spinScale_valueChanged\0arg1\0"
+    "on_spinInterval_valueChanged\0"
+    "on_spinCount_valueChanged\0"
+    "on_btnBackgroundColor_clicked\0"
+    "on_btnLineColor_clicked\0on_btnCircle_clicked\0"
+    "on_btnEllipse_clicked\0on_btnFunny_clicked\0"
+    "on_btnStar_clicked"
 };
 #undef QT_MOC_LITERAL
 
@@ -51,7 +69,7 @@ static const uint qt_meta_data_MainWindow[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-       4,   14, // methods
+      14,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -59,12 +77,32 @@ static const uint qt_meta_data_MainWindow[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   34,    2, 0x08 /* Private */,
-       3,    0,   35,    2, 0x08 /* Private */,
-       4,    0,   36,    2, 0x08 /* Private */,
-       5,    0,   37,    2, 0x08 /* Private */,
+       1,    0,   84,    2, 0x08 /* Private */,
+       3,    0,   85,    2, 0x08 /* Private */,
+       4,    0,   86,    2, 0x08 /* Private */,
+       5,    0,   87,    2, 0x08 /* Private */,
+       6,    0,   88,    2, 0x08 /* Private */,
+       7,    1,   89,    2, 0x08 /* Private */,
+       9,    1,   92,    2, 0x08 /* Private */,
+      10,    1,   95,    2, 0x08 /* Private */,
+      11,    0,   98,    2, 0x08 /* Private */,
+      12,    0,   99,    2, 0x08 /* Private */,
+      13,    0,  100,    2, 0x08 /* Private */,
+      14,    0,  101,    2, 0x08 /* Private */,
+      15,    0,  102,    2, 0x08 /* Private */,
+      16,    0,  103,    2, 0x08 /* Private */,
 
  // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Double,    8,
+    QMetaType::Void, QMetaType::Double,    8,
+    QMetaType::Void, QMetaType::Int,    8,
+    QMetaType::Void,
+    QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
@@ -83,10 +121,19 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         case 1: _t->on_btnCycloid_clicked(); break;
         case 2: _t->on_btnHuygens_clicked(); break;
         case 3: _t->on_btnHypo_clicked(); break;
+        case 4: _t->on_btnLine_clicked(); break;
+        case 5: _t->on_spinScale_valueChanged((*reinterpret_cast< double(*)>(_a[1]))); break;
+        case 6: _t->on_spinInterval_valueChanged((*reinterpret_cast< double(*)>(_a[1]))); break;
+        case 7: _t->on_spinCount_valueChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 8: _t->on_btnBackgroundColor_clicked(); break;
+        case 9: _t->on_btnLineColor_clicked(); break;
+        case 10: _t->on_btnCircle_clicked(); break;
+        case 11: _t->on_btnEllipse_clicked(); break;
+        case 12: _t->on_btnFunny_clicked(); break;
+        case 13: _t->on_btnStar_clicked(); break;
         default: ;
         }
     }
-    Q_UNUSED(_a);
 }
 
 const QMetaObject MainWindow::staticMetaObject = {
@@ -114,13 +161,13 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 4)
+        if (_id < 14)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 4;
+        _id -= 14;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 4)
+        if (_id < 14)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 4;
+        _id -= 14;
     }
     return _id;
 }
--- build-qt-curves-Desktop_Qt_5_10_0_clang_64bit-Debug/ui_mainwindow.h
+++ build-qt-curves-Desktop_Qt_5_10_0_clang_64bit-Debug/ui_mainwindow.h
@@ -13,11 +13,14 @@
 #include <QtWidgets/QAction>
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QDoubleSpinBox>
 #include <QtWidgets/QHBoxLayout>
 #include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLabel>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QPushButton>
 #include <QtWidgets/QSpacerItem>
+#include <QtWidgets/QSpinBox>
 #include <QtWidgets/QStatusBar>
 #include <QtWidgets/QVBoxLayout>
 #include <QtWidgets/QWidget>
@@ -29,66 +32,189 @@ class Ui_MainWindow
 {
 public:
     QWidget *centralWidget;
-    QWidget *widget;
-    QHBoxLayout *horizontalLayout;
+    QHBoxLayout *horizontalLayout_5;
+    QVBoxLayout *verticalLayout_2;
     RenderArea *renderArea;
+    QHBoxLayout *horizontalLayout_4;
+    QPushButton *btnBackgroundColor;
+    QPushButton *btnLineColor;
+    QSpacerItem *horizontalSpacer;
     QVBoxLayout *verticalLayout;
     QPushButton *btnAstroid;
     QPushButton *btnCycloid;
     QPushButton *btnHuygens;
     QPushButton *btnHypo;
-    QSpacerItem *verticalSpacer;
+    QPushButton *btnLine;
+    QPushButton *btnCircle;
+    QPushButton *btnEllipse;
+    QPushButton *btnFunny;
+    QPushButton *btnStar;
+    QHBoxLayout *horizontalLayout;
+    QLabel *label;
+    QDoubleSpinBox *spinScale;
+    QHBoxLayout *horizontalLayout_2;
+    QLabel *label_2;
+    QDoubleSpinBox *spinInterval;
+    QHBoxLayout *horizontalLayout_3;
+    QLabel *label_3;
+    QSpinBox *spinCount;
     QStatusBar *statusBar;
 
     void setupUi(QMainWindow *MainWindow)
     {
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName(QStringLiteral("MainWindow"));
-        MainWindow->resize(496, 352);
+        MainWindow->resize(528, 398);
         centralWidget = new QWidget(MainWindow);
         centralWidget->setObjectName(QStringLiteral("centralWidget"));
-        widget = new QWidget(centralWidget);
-        widget->setObjectName(QStringLiteral("widget"));
-        widget->setGeometry(QRect(13, 13, 471, 301));
-        horizontalLayout = new QHBoxLayout(widget);
-        horizontalLayout->setSpacing(6);
-        horizontalLayout->setContentsMargins(11, 11, 11, 11);
-        horizontalLayout->setObjectName(QStringLiteral("horizontalLayout"));
-        horizontalLayout->setContentsMargins(0, 0, 0, 0);
-        renderArea = new RenderArea(widget);
+        horizontalLayout_5 = new QHBoxLayout(centralWidget);
+        horizontalLayout_5->setSpacing(6);
+        horizontalLayout_5->setContentsMargins(11, 11, 11, 11);
+        horizontalLayout_5->setObjectName(QStringLiteral("horizontalLayout_5"));
+        verticalLayout_2 = new QVBoxLayout();
+        verticalLayout_2->setSpacing(6);
+        verticalLayout_2->setObjectName(QStringLiteral("verticalLayout_2"));
+        renderArea = new RenderArea(centralWidget);
         renderArea->setObjectName(QStringLiteral("renderArea"));
 
-        horizontalLayout->addWidget(renderArea);
+        verticalLayout_2->addWidget(renderArea);
+
+        horizontalLayout_4 = new QHBoxLayout();
+        horizontalLayout_4->setSpacing(6);
+        horizontalLayout_4->setObjectName(QStringLiteral("horizontalLayout_4"));
+        btnBackgroundColor = new QPushButton(centralWidget);
+        btnBackgroundColor->setObjectName(QStringLiteral("btnBackgroundColor"));
+
+        horizontalLayout_4->addWidget(btnBackgroundColor);
+
+        btnLineColor = new QPushButton(centralWidget);
+        btnLineColor->setObjectName(QStringLiteral("btnLineColor"));
+
+        horizontalLayout_4->addWidget(btnLineColor);
+
+        horizontalSpacer = new QSpacerItem(68, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+        horizontalLayout_4->addItem(horizontalSpacer);
+
+
+        verticalLayout_2->addLayout(horizontalLayout_4);
+
+
+        horizontalLayout_5->addLayout(verticalLayout_2);
 
         verticalLayout = new QVBoxLayout();
         verticalLayout->setSpacing(6);
         verticalLayout->setObjectName(QStringLiteral("verticalLayout"));
-        btnAstroid = new QPushButton(widget);
+        btnAstroid = new QPushButton(centralWidget);
         btnAstroid->setObjectName(QStringLiteral("btnAstroid"));
 
         verticalLayout->addWidget(btnAstroid);
 
-        btnCycloid = new QPushButton(widget);
+        btnCycloid = new QPushButton(centralWidget);
         btnCycloid->setObjectName(QStringLiteral("btnCycloid"));
 
         verticalLayout->addWidget(btnCycloid);
 
-        btnHuygens = new QPushButton(widget);
+        btnHuygens = new QPushButton(centralWidget);
         btnHuygens->setObjectName(QStringLiteral("btnHuygens"));
 
         verticalLayout->addWidget(btnHuygens);
 
-        btnHypo = new QPushButton(widget);
+        btnHypo = new QPushButton(centralWidget);
         btnHypo->setObjectName(QStringLiteral("btnHypo"));
 
         verticalLayout->addWidget(btnHypo);
 
-        verticalSpacer = new QSpacerItem(78, 28, QSizePolicy::Minimum, QSizePolicy::Expanding);
+        btnLine = new QPushButton(centralWidget);
+        btnLine->setObjectName(QStringLiteral("btnLine"));
 
-        verticalLayout->addItem(verticalSpacer);
+        verticalLayout->addWidget(btnLine);
+
+        btnCircle = new QPushButton(centralWidget);
+        btnCircle->setObjectName(QStringLiteral("btnCircle"));
+
+        verticalLayout->addWidget(btnCircle);
+
+        btnEllipse = new QPushButton(centralWidget);
+        btnEllipse->setObjectName(QStringLiteral("btnEllipse"));
+
+        verticalLayout->addWidget(btnEllipse);
+
+        btnFunny = new QPushButton(centralWidget);
+        btnFunny->setObjectName(QStringLiteral("btnFunny"));
+
+        verticalLayout->addWidget(btnFunny);
+
+        btnStar = new QPushButton(centralWidget);
+        btnStar->setObjectName(QStringLiteral("btnStar"));
+
+        verticalLayout->addWidget(btnStar);
+
+        horizontalLayout = new QHBoxLayout();
+        horizontalLayout->setSpacing(6);
+        horizontalLayout->setObjectName(QStringLiteral("horizontalLayout"));
+        label = new QLabel(centralWidget);
+        label->setObjectName(QStringLiteral("label"));
+        label->setAlignment(Qt::AlignRight|Qt::AlignTrailing|Qt::AlignVCenter);
+
+        horizontalLayout->addWidget(label);
+
+        spinScale = new QDoubleSpinBox(centralWidget);
+        spinScale->setObjectName(QStringLiteral("spinScale"));
+        spinScale->setMinimumSize(QSize(60, 20));
+        spinScale->setMaximumSize(QSize(60, 20));
+        spinScale->setDecimals(1);
+        spinScale->setSingleStep(0.1);
+
+        horizontalLayout->addWidget(spinScale);
 
 
-        horizontalLayout->addLayout(verticalLayout);
+        verticalLayout->addLayout(horizontalLayout);
+
+        horizontalLayout_2 = new QHBoxLayout();
+        horizontalLayout_2->setSpacing(6);
+        horizontalLayout_2->setObjectName(QStringLiteral("horizontalLayout_2"));
+        label_2 = new QLabel(centralWidget);
+        label_2->setObjectName(QStringLiteral("label_2"));
+        label_2->setAlignment(Qt::AlignRight|Qt::AlignTrailing|Qt::AlignVCenter);
+
+        horizontalLayout_2->addWidget(label_2);
+
+        spinInterval = new QDoubleSpinBox(centralWidget);
+        spinInterval->setObjectName(QStringLiteral("spinInterval"));
+        spinInterval->setMinimumSize(QSize(60, 20));
+        spinInterval->setMaximumSize(QSize(60, 20));
+        spinInterval->setDecimals(2);
+        spinInterval->setSingleStep(0.1);
+
+        horizontalLayout_2->addWidget(spinInterval);
+
+
+        verticalLayout->addLayout(horizontalLayout_2);
+
+        horizontalLayout_3 = new QHBoxLayout();
+        horizontalLayout_3->setSpacing(6);
+        horizontalLayout_3->setObjectName(QStringLiteral("horizontalLayout_3"));
+        label_3 = new QLabel(centralWidget);
+        label_3->setObjectName(QStringLiteral("label_3"));
+        label_3->setAlignment(Qt::AlignRight|Qt::AlignTrailing|Qt::AlignVCenter);
+
+        horizontalLayout_3->addWidget(label_3);
+
+        spinCount = new QSpinBox(centralWidget);
+        spinCount->setObjectName(QStringLiteral("spinCount"));
+        spinCount->setMinimumSize(QSize(60, 20));
+        spinCount->setMaximumSize(QSize(60, 20));
+        spinCount->setMaximum(512);
+        spinCount->setSingleStep(8);
+
+        horizontalLayout_3->addWidget(spinCount);
+
+
+        verticalLayout->addLayout(horizontalLayout_3);
+
+
+        horizontalLayout_5->addLayout(verticalLayout);
 
         MainWindow->setCentralWidget(centralWidget);
         statusBar = new QStatusBar(MainWindow);
@@ -103,10 +229,20 @@ public:
     void retranslateUi(QMainWindow *MainWindow)
     {
         MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", nullptr));
+        btnBackgroundColor->setText(QApplication::translate("MainWindow", "Background Color", nullptr));
+        btnLineColor->setText(QApplication::translate("MainWindow", "Line Color", nullptr));
         btnAstroid->setText(QApplication::translate("MainWindow", "Astroid", nullptr));
         btnCycloid->setText(QApplication::translate("MainWindow", "Cycloid", nullptr));
         btnHuygens->setText(QApplication::translate("MainWindow", "Huygens", nullptr));
         btnHypo->setText(QApplication::translate("MainWindow", "Hypo Cycloid", nullptr));
+        btnLine->setText(QApplication::translate("MainWindow", "Line", nullptr));
+        btnCircle->setText(QApplication::translate("MainWindow", "Circle", nullptr));
+        btnEllipse->setText(QApplication::translate("MainWindow", "Ellipse", nullptr));
+        btnFunny->setText(QApplication::translate("MainWindow", "Funny", nullptr));
+        btnStar->setText(QApplication::translate("MainWindow", "Star", nullptr));
+        label->setText(QApplication::translate("MainWindow", "Scale :", nullptr));
+        label_2->setText(QApplication::translate("MainWindow", "Interval Length :", nullptr));
+        label_3->setText(QApplication::translate("MainWindow", "Step Count :", nullptr));
     } // retranslateUi
 
 };
